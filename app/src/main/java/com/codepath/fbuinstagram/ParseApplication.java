97d9d1320898ec2f8235728147a40f814d36c6ed
riveraluis <85712545+riveraluis@users.noreply.github.com>
2021-07-07 18:16:05 -0400

    User can post photos successfully
    - User can add description, and take a picture.

--- app/src/main/java/com/codepath/fbuinstagram/ParseApplication.java
+++ app/src/main/java/com/codepath/fbuinstagram/ParseApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
@@ -11,6 +12,9 @@ public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         // Register your Parse models
         Parse.initialize(new Parse.Configuration.Builder(this)
